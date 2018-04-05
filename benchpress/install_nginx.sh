58d271a05ba8bfcfa0743ed30d9284bb7364b1d5
Carlos Torres <cltorres@fb.com>
2018-04-05 15:05:34 -0700

    [benchpress] Add nginx installation scripts

---
+++ benchpress/install_nginx.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+set -x
+
+NGINX_VERSION='nginx-1.12.2'
+NGINX_TAR_FILE="${NGINX_VERSION}.tar.gz"
+NGINX_DOWNLOAD_URL="http://nginx.org/download/${NGINX_TAR_FILE}"
+
+PCRE_VERSION='pcre-8.41'
+PCRE_TAR_FILE="${PCRE_VERSION}.tar.gz"
+PCRE_DOWNLOAD_URL="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/${PCRE_TAR_FILE}"
+
+ZLIB_VERSION='zlib-1.2.11'
+ZLIB_TAR_FILE="${ZLIB_VERSION}.tar.gz"
+ZLIB_DOWNLOAD_URL="http://zlib.net/${ZLIB_TAR_FILE}"
+
+BENCHMARKS_DIR="$(pwd)/benchmarks"
+mkdir -p "$BENCHMARKS_DIR"
+
+rm -rf build
+mkdir -p build
+cd build
+
+echo 'Installing NGINX dependencies'
+
+#PCRE
+wget "${PCRE_DOWNLOAD_URL}"
+tar -zxvf "$PCRE_TAR_FILE"
+cd "$PCRE_VERSION"
+./configure
+make -j4
+cd ../
+
+#zlib
+wget "${ZLIB_DOWNLOAD_URL}"
+tar -zxvf "$ZLIB_TAR_FILE"
+cd "$ZLIB_VERSION"
+./configure
+make -j4
+cd ../
+
+echo 'Done installing NGINX dependencies'
+echo 'Installing NGINX'
+#NGINX
+wget "${NGINX_DOWNLOAD_URL}"
+tar -xzvf "$NGINX_TAR_FILE"
+
+cd "$NGINX_VERSION"
+./configure \
+    --prefix="${BENCHMARKS_DIR}/nginx" \
+    --with-pcre="../${PCRE_VERSION}" \
+    --with-zlib="../${ZLIB_VERSION}" \
+    --with-http_ssl_module
+
+make -j4
+make install
+
+cd ../../
+rm -rf build
+
+echo "NGINX installed into ${BENCHMARKS_DIR}"
