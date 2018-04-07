e226c120577814b6d241b028ccf1c8c45f071cb8
Carlos Torres <cltorres@fb.com>
2018-04-06 17:18:04 -0700

    [benchpress] Check and stop if earlier nginx instance is running

--- benchpress/templates/nginx_wrk_bench.sh
+++ benchpress/templates/nginx_wrk_bench.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e  # Exit on error
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 NGINX_CONF_FILE="$( readlink -m "${DIR}/../templates/nginx.conf" )"
+set +e
+
+if [ -f "${DIR}/nginx/logs/nginx.pid" ]; then
+  echo 'NGINX is already running'
+  echo 'Stopping NGINX'
+  "${DIR}/nginx/sbin/nginx" -s quit
+fi
+set -e
 
 echo 'Starting NGINX server'
 "${DIR}/nginx/sbin/nginx" -c "$NGINX_CONF_FILE"
 
-set +e  # Don't exit on error to be able to stop nginx
+set +e # Don't exit on error, so we can gracefully shutdown nginx
 echo 'Executing wrk'
 "${DIR}/wrk" "$@"
 WRK_EXIT_CODE=$?
